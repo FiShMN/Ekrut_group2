e450b4decdcccf3ce9c4590bb1c0d46fa166bb4d
USER <USER@DESKTOP-KMG62J9>
2022-12-11 16:26:24 +0200

    added shared folder

---
+++ SharedEkrut/src/Entities/MessageType.java
@@ -0,0 +1,21 @@
+package Entities;
+
+public enum MessageType {
+	//server messages
+	Updated_successfully,show_orders_successfull,login,disconnected,Already_Logged_In,Login_Wrong_Input,
+	Add_product_succ,Add_product_fail,Customer_list,Customer_list_update_Succ,Customer_registration_approved,
+	Customer_registration_not_approved,Show_products_succ, download_report_pdf_succ,get_recipt,Orders_list,
+	order_approved,
+	
+	//client messages
+	Show_all_monthly_orders_for_subscriber,Show_orders_history,Show_vending_machines,Add_product,
+	Delete_Account,Check_account_existance, Show_products,Delete_product,Product_delete_succ,Get_orders_report,
+	Order_delivered,Download_PDF_Report,Change_status_to_delivered_Succ,Order_Done,Show_Histogram,
+	
+	//types of users
+	Customer,ClubMember,CEO,RegionManager,StockWorker,CustomerServiceWorker,
+	
+	//Error
+	Error;
+	
+}
---
+++ SharedEkrut/src/Entities/Product.java
@@ -0,0 +1,34 @@
+package Entities;
+import java.io.Serializable;
+
+
+public class Product implements Serializable {
+	private String productName,productCode;
+	private float price;
+	private int quantity;
+	public Product(String productName, String productCode, float price) {
+		this.productName=productName;
+		this.productCode=productCode;
+		this.price=price;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public String getProductCode() {
+		return productCode;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity=quantity;
+	}
+	@Override
+	public String toString() {
+		return "Product [name: "+productName+" code: "+productCode+" price for one:"
+				+ " "+price+" quantity: "+quantity+"final price: "+price*quantity+"]";
+	}
+}
