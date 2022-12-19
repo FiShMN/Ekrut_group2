942501444d84871da356bd3c336c2f455cd95bdb
AsafSchneiderman <aassaaff323@gmail.com>
2022-12-19 22:07:08 +0200

    I Updated the constructor of Worker in entities in SharedEkrut

--- SharedEkrut/src/Entities/Worker.java
+++ SharedEkrut/src/Entities/Worker.java
@@ -1,12 +1,14 @@
 package Entities;
 import java.io.Serializable;
-public class Worker extends User{
+public class Worker extends User implements Serializable{
+	
+	private static final long serialVersionUID = 7813368042717409702L;
 	private String jobType;
 	private String region;
 
-	public Worker(String name, String lastName, String id, String phoneNumber, String email, String userName, String password,
-			String permissions, String jobType, String region) {
-		super(name, lastName, id, phoneNumber, email, userName, password, permissions);
+	public Worker(String Fname, String lastName, String id, String phoneNumber, String email, String userName, String password,
+			String role,boolean isLoggedIn, String jobType, String region) {
+		super(Fname, lastName, id, phoneNumber, email, userName, password, role,isLoggedIn);
 		this.jobType = jobType;
 		this.region = region;	
 	}
